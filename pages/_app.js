9242fee833f85e31419c6448970449017f32338f
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-11-30 17:55:07 +0530

    Project Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Root from '../src/redux/Root';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <Root>
+      <Component {...pageProps} />
+    </Root>
+  );
 }
 
-export default MyApp
+export default App;
